48765633b01e1cc84ca773ed860f3b520067c156
jrquadros <jrmquadros@gmail.com>
2022-04-24 17:42:12 -0300

    chore: update environment type

--- environment.d.ts
+++ environment.d.ts
@@ -3,6 +3,9 @@ declare global {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production' | 'test';
       SERVER_PORT: number;
+      DB_USER: string;
+      POSTGRES_PASSWORD: string;
+      POSTGRES_DB: string;
     }
   }
 }
