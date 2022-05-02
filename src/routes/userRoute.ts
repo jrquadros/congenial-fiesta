8f033bb549f14f3b3abbfbe51fdbc083d0918e04
jrquadros <jrmquadros@gmail.com>
2022-05-02 10:36:21 -0300

    feat(user): get info

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { userController } from '../controllers/user/userController';
-import { verifyToken } from '../middlewares/extractToken';
+import { verifyToken } from '../middlewares/verifyToken';
 
 const userRoute = express
   .Router()
   .post('/register', userController.register)
   .post('/auth', userController.auth)
-  .delete('/', verifyToken, userController.deleteAccount);
+  .delete('/', verifyToken, userController.deleteAccount)
+  .get('/', verifyToken, userController.userInfo);
 
 export { userRoute };
