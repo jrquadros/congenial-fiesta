fdf7c4f531b0e5710a1100ab016588e0df67b557
jrquadros <jrmquadros@gmail.com>
2022-05-01 23:53:35 -0300

    feat(user): delete account

---
+++ src/controllers/user/deleteAccount.ts
@@ -0,0 +1,23 @@
+import { RequestHandler } from 'express';
+import { userRepository } from '../../repositories/userRepository';
+import { getTokenPayload } from '../../utils/getTokenPayload';
+
+export const deleteAccount: RequestHandler = async (req, res) => {
+  const token = req.headers.token;
+
+  const payload = getTokenPayload(token as string);
+
+  const { id } = payload;
+
+  try {
+    const deletedUser = await userRepository.delete({ id });
+
+    if (!deletedUser) {
+      return res.status(404).json({ error: 'User not found' }).send();
+    }
+
+    return res.status(204).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal server error' }).send();
+  }
+};
