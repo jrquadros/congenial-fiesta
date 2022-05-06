9e89fb06d18721de778a209205af61b50a9fbe36
jrquadros <jrmquadros@gmail.com>
2022-05-06 15:53:49 -0300

    chore(routes): add address nested route

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { userController } from '../controllers/user/userController';
 import { verifyToken } from '../middlewares/verifyToken';
+import { addressRouter } from './addressRoute';
 
 const userRoute = express
   .Router()
   .post('/register', userController.register)
   .post('/auth', userController.auth)
   .delete('/', verifyToken, userController.deleteAccount)
-  .get('/', verifyToken, userController.userInfo);
+  .get('/', verifyToken, userController.userInfo)
+  .use('/address', addressRouter);
 
 export { userRoute };
