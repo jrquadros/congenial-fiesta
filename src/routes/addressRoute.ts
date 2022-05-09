04c96bbc312997fa42364ceaaf7884643c46bdc7
jrquadros <jrmquadros@gmail.com>
2022-05-09 18:06:57 -0300

    feat(router): add get addresses route

--- src/routes/addressRoute.ts
+++ src/routes/addressRoute.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import { verifyToken } from '../middlewares/verifyToken';
 import { addressController } from '../controllers/address/addressController';
 
-const addressRouter = express.Router().post('/', verifyToken, addressController.createAddress);
+const addressRouter = express
+  .Router()
+  .post('/', verifyToken, addressController.createAddress)
+  .get('/', verifyToken, addressController.getAddresses);
 
 export { addressRouter };
